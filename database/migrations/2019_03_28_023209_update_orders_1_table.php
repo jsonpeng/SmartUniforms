027b9281e400d563ed9f47db83e649b887f373ae
jsonpeng <1127915086@qq.com>
2019-03-28 16:51:47 +0800

    加入快递方式切换

--- database/migrations/2019_03_28_023209_update_orders_1_table.php
+++ database/migrations/2019_03_28_023209_update_orders_1_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class UpdateOrdersTable1 extends Migration
+class UpdateOrders1Table extends Migration
 {
 
     /**

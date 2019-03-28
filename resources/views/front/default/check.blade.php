dfbf63350dc85314bb5235640eff3839f37f80e8
jsonpeng <1127915086@qq.com>
2019-03-28 16:30:19 +0800

    ss

--- resources/views/front/default/check.blade.php
+++ resources/views/front/default/check.blade.php
@@ -204,13 +204,7 @@
 
     <div class="page__bd section-margin bill">
         <div class="weui-cells weui-cells_form">
-            <div class="weui-cell weui-cell_switch">
-              {{-- <div class="weui-cell__bd">快递方式</div> --}}
-              <div class="weui-cell__ft">
-                 {{--  <input type="checkbox" class="weui-switch" id="invoice" name="invoice"> --}}
-              </div>
-            </div>
-            <div id="invoice_detail" >
+            <div id="kuaidi-way" >
               <div class="weui-cell">
                   <div class="fistname">请选择快递方式</div>
               </div>

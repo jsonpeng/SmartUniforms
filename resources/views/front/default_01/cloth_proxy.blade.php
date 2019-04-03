d3ef79b32096406d30eb731d193cc8bc54ae1dbd
jsonpeng <1127915086@qq.com>
2019-04-03 17:58:35 +0800

    添加校服代理页面

--- resources/views/front/default_01/cloth_proxy.blade.php
+++ resources/views/front/default_01/cloth_proxy.blade.php
@@ -11,8 +11,8 @@
 @endsection
 
 @section('content')
-<div ><img src="{!! asset('images/proxy1.png') !!}" style="width: 100%;height: auto;" /></div>
-<div ><img src="{!! asset('images/proxy2.png') !!}" style="width: 100%;height: auto;" /></div>
+<div ><img src="{!! asset('images/proxy1.png') !!}" style="width: 100%;height: auto;    margin-bottom: -5px;" /></div>
+<div ><img src="{!! asset('images/proxy2.png') !!}" style="width: 100%;height: auto;    margin-bottom: -5px;" /></div>
 <a href="tel:{!! getSettingValueByKeyCache('service_tel') !!}" style="display: block;">
     <img src="{!! asset('images/proxy.png') !!}" style="width: 100%;height: auto;" class="proxy_img" />
   {{--   <a class="proxy_a_tel" href="tel:{!! getSettingValueByKeyCache('service_tel') !!}" style="display: none;">t</a> --}}

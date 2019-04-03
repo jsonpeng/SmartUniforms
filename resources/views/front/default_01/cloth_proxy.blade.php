8fd89fc37a2ab72bc1a3d932ad821617cb10c66c
jsonpeng <1127915086@qq.com>
2019-04-03 17:54:19 +0800

    添加校服代理页面

--- resources/views/front/default_01/cloth_proxy.blade.php
+++ resources/views/front/default_01/cloth_proxy.blade.php
@@ -7,10 +7,12 @@
 @endsection
 
 @section('title')
-<title>四季校服 专业校服定制 校服代理</title>
+<title>四季校服 专业校服定制</title>
 @endsection
 
 @section('content')
+<div ><img src="{!! asset('images/proxy1.png') !!}" style="width: 100%;height: auto;" /></div>
+<div ><img src="{!! asset('images/proxy2.png') !!}" style="width: 100%;height: auto;" /></div>
 <a href="tel:{!! getSettingValueByKeyCache('service_tel') !!}" style="display: block;">
     <img src="{!! asset('images/proxy.png') !!}" style="width: 100%;height: auto;" class="proxy_img" />
   {{--   <a class="proxy_a_tel" href="tel:{!! getSettingValueByKeyCache('service_tel') !!}" style="display: none;">t</a> --}}

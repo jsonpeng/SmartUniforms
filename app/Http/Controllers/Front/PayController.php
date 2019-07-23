fe4abe374dbfbe754a986f0807e8a56030cbe76e
HipePeng(彭云) <1127915086@qq.com>
2019-07-23 17:15:11 +0800

    加入paysapi支付
    加入paysapi支付

--- app/Http/Controllers/Front/PayController.php
+++ app/Http/Controllers/Front/PayController.php
@@ -22,6 +22,113 @@ class PayController extends Controller
     {
         $this->orderRepository = $orderRepo;
     }
+    
+   /**
+    *PAYS API支付
+    */
+    public function paysApi(Request $request,$price)
+    {
+        $out_trade_no = time();
+        $body = '支付订单'.$$out_trade_no.'费用';
+        $user =auth('web')->user();
+
+        $orderuid = $user->id;       //此处传入您网站用户的用户名，方便在paysapi后台查看是谁付的款，强烈建议加上。可忽略。
+
+        //校验传入的表单，确保价格为正常价格（整数，1位小数，2位小数都可以），支付渠道只能是1或者2，orderuid长度不要超过33个中英文字。
+        $istype = 2;
+        if ($request->has('type') && $request->input('type') == 1) {
+            $istype = 1;
+        }
+
+        //此处就在您服务器生成新订单，并把创建的订单号传入到下面的orderid中。
+        $goodsname = $body;
+        $orderid = $out_trade_no;    //每次有任何参数变化，订单号就变一个吧。
+        $uid = Config::get('paysapi.PAYS_API_UID');//"此处填写PaysApi的uid";
+        $token = Config::get('paysapi.PAYS_API_TOKEN');//"此处填写PaysApi的Token";
+        $return_url = $request->root().'/paysapi_return';
+        $notify_url = $request->root().'/paysapi_notify';
+        
+        $key = md5($goodsname. $istype . $notify_url . $orderid . $orderuid . $price . $return_url . $token . $uid);
+        //经常遇到有研发问为啥key值返回错误，大多数原因：1.参数的排列顺序不对；2.上面的参数少传了，但是这里的key值又带进去计算了，导致服务端key算出来和你的不一样。
+
+        $returndata['goodsname'] = $goodsname;
+        $returndata['istype'] = $istype;
+        $returndata['key'] = $key;
+        $returndata['notify_url'] = $notify_url;
+        $returndata['orderid'] = $orderid;
+        $returndata['orderuid'] =$orderuid;
+        $returndata['price'] = $price;
+        $returndata['return_url'] = $return_url;
+        $returndata['uid'] = $uid;
+        return ['code' => 0, 'message' => $returndata];
+    }
+
+    /**
+     * PAYS API返回
+     * @param    Request     $request [description]
+     * @return   [type]               [description]
+     */
+    public function paysapiReturn(Request $request)
+    {
+        $inputs = $request->all();
+        $orderid = $inputs["orderid"];
+        return view(frontView('pay.pay_success'));
+    }
+
+    /**
+     * PAYS API 返回
+     * @param    Request     $request [description]
+     * @return   [type]               [description]
+     */
+    public function paysapiNotify(Request $request)
+    {
+        $inputs = $request->all();
+        $paysapi_id = $inputs["paysapi_id"];
+        $orderid = $inputs["orderid"];
+        $price = $inputs["price"];
+        $realprice = $inputs["realprice"];
+        $orderuid = $inputs["orderuid"];
+        $key = $inputs["key"];
+
+        //校验传入的参数是否格式正确，略
+
+        $token = Config::get('paysapi.PAYS_API_TOKEN');
+        
+        $temps = md5($orderid . $orderuid . $paysapi_id . $price . $realprice . $token);
+
+        if ($temps != $key){
+            return $this->jsonError("key值不匹配");
+        }else{
+           #支付成功
+        }
+
+    }
+
+    /**
+     * PAYS API返回
+     */
+    //返回错误
+    private function jsonError($message = '',$url=null) 
+    {
+        $return['msg'] = $message;
+        $return['data'] = '';
+        $return['code'] = -1;
+        $return['url'] = $url;
+        return json_encode($return);
+    }
+
+    /**
+     * PAYS API返回
+     */
+    //返回正确
+    private function jsonSuccess($message = '',$data = '',$url=null) 
+    {
+        $return['msg']  = $message;
+        $return['data'] = $data;
+        $return['code'] = 1;
+        $return['url'] = $url;
+        return json_encode($return);
+    }
 	
     //支付征订单
     public function payZhengDing(Request $request,$price)
